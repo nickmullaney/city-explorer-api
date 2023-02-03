03f582eab0c3e8250ed3a461a0a64e1b9f46390a
nickmullaney <nick.mullaney@gmail.com>
2023-02-02 20:13:28 -0800

    updated with movies

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/weather', async(req, res, next) => {
     // city is our new search query
     let city = req.query.searchQuery;
     // this is our URL to take us to the live weather API
-    let liveWeatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${process.env.REACT_APP_WEATHER_API_KEY}`;
+    let liveWeatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${process.env.REACT_APP_WEATHER_API_KEY}&days=10`;
     // response to get the live weather data
     let response = await axios.get(liveWeatherUrl);
     console.log(response.data);
@@ -62,15 +62,15 @@ app.get('/movies', async(req, res, next) => {
  
   try {
     // city is our new search query
-    let city = req.query.searchQuery;
+    let movie = req.query.searchQuery;
     // this is our URL to take us to the live weather API
-    let liveMovieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&query=${city}`
-    
+    let liveMovieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&query=${movie}`
+    console.log("Live Movie URL",liveMovieUrl)
     // response to get the live weather data
-    let response = await axios.get(liveMoviesUrl);
+    let response = await axios.get(liveMovieUrl);
     console.log(response.data);
     // function to feed the weather data from the specific days into our Forcast Class
-    let descriptions = response.data.data.map(day=> new Forecast(day));
+    let descriptions = response.data.results.map(movie=> new Movies(movie));
     // Send out our descriptions data
     res.status(200).send(descriptions);
   }
@@ -80,9 +80,14 @@ app.get('/movies', async(req, res, next) => {
 });
 
 class Movies{
-  constructor(city){
-    this.
-  }
+  constructor(movie){
+    this.title = movie.title,
+    this.overview = movie.overview,
+    this.average_votes = movie.vote_average,
+    this.poster = movie.poster_path,
+    this.popularity = movie.popularity,
+    this.released_on = movie.release_date
+  };
 }
 
 class Forecast {
